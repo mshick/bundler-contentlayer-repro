b56b6ccae00546e1fc4309d09664046fb9050f3e
Michael Shick <m@shick.us>
2022-03-28 12:06:59 -0400

    Create contentlayer.config.ts

---
+++ contentlayer.config.ts
@@ -0,0 +1,51 @@
+import {
+  defineDocumentType,
+  makeSource,
+} from 'contentlayer/source-files'
+
+export const PostMDX = defineDocumentType(() => ({
+  name: 'PostMDX',
+  filePathPattern: '**/*.mdx',
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true }
+  }
+}))
+
+export const PostMD = defineDocumentType(() => ({
+  name: 'PostMD',
+  filePathPattern: '**/*.md',
+  contentType: 'md',
+  fields: {
+    title: { type: 'string', required: true }
+  }
+}))
+
+export default makeSource({
+  contentDirPath: 'data',
+  documentTypes: [PostMDX, PostMD],
+  // Assuming a Markdown bundler works similar to mdx-bundler, the config behavior would be similar for both
+  // md: (context) => {
+  mdx: (context) => {
+    return {
+      cwd: path.dirname(context.filePath),
+      remarkPlugins: [
+        remarkMdxImages,
+      ],
+      esbuildOptions: (options) => {
+        options.platform = 'node'
+        options.outdir = `./public`
+        options.assetNames = `images/[dir]/[name]`
+        options.loader = {
+          ...options.loader,
+          '.png': 'file',
+          '.jpg': 'file',
+          '.jpeg': 'file',
+        }
+        options.publicPath = '/'
+        options.write = true
+        return options
+      }
+    }
+  }
+})
